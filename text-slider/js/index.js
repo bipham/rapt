b7d8b545579776834f0cd4cda74f2f777081f754
kyo kun <kyo kun>
2017-03-28 00:20:51 +0700

    fix

--- text-slider/js/index.js
+++ text-slider/js/index.js
@@ -6,12 +6,13 @@ var isHover = false;
 var slideIndex = 0;
 var sliderDetail;
 var sliderDetailTotal;
+var oldIndexOverview;
 var slider = $('.bxslider.slider-overview').bxSlider({
     pagerCustom: '#bx-pager',
     startSlide: slideIndex,
     slideMargin: 10,
     pagerType: 'full',
-    controls: false, 
+    controls: false,
     onSlideAfter: function($slideElement, oldIndex, newIndex) {
         var liSelected = 'li-img-' + newIndex;
         var $this = $('li.' + liSelected)
@@ -25,12 +26,11 @@ $('ul li').each(function(i) {
     $(this).attr('rel'); // This is your rel value
 });
 
-$('ul.bxslider.slider-overview').hover(function(){
+$('ul.bxslider.slider-overview').hover(function() {
     if (isHover == false) {
         alert('www');
         isHover = true;
-    }
-    else return;
+    } else return;
 });
 
 $(function() {
@@ -63,22 +63,35 @@ $(".img-drop").droppable({
         // slider.goToSlide(slideIndex);
         slideIndex = $(this).parent().attr('data-slide-index');
         $(this).addClass('img-visiting');
-        // slider.reloadSlider();
-        slider.destroySlider();
-        slider = $('.bxslider.slider-overview').bxSlider({
+        slider.reloadSlider({
             pagerCustom: '#bx-pager',
             startSlide: slideIndex,
             // slideWidth: 100,
             slideMargin: 10,
-             controls: false, 
-            pagerType: 'full', 
+            controls: false,
+            pagerType: 'full',
             onSlideAfter: function($slideElement, oldIndex, newIndex) {
                 var liSelected = 'li-img-' + newIndex;
                 var $this = $('li.' + liSelected)
                 chooseSlideImage($this, oldIndex);
             },
-            speed: 10
+            speed: 100
         });
+        // slider.destroySlider();
+        // slider = $('.bxslider.slider-overview').bxSlider({
+        //     pagerCustom: '#bx-pager',
+        //     startSlide: slideIndex,
+        //     // slideWidth: 100,
+        //     slideMargin: 10,
+        //      controls: false, 
+        //     pagerType: 'full', 
+        //     onSlideAfter: function($slideElement, oldIndex, newIndex) {
+        //         var liSelected = 'li-img-' + newIndex;
+        //         var $this = $('li.' + liSelected)
+        //         chooseSlideImage($this, oldIndex);
+        //     },
+        //     speed: 10
+        // });
     },
     out: function(event, ui) {
         // $(this).parent().attr('class', '');
@@ -104,7 +117,7 @@ $(".img-drop").droppable({
         // slider.goToSlide(slideIndex);
     }
 });
-$('ul.thumbnail-list-img li').click(function () {
+$('ul.thumbnail-list-img li').click(function() {
     var oldIndex = slider.getCurrentSlide();
     chooseThumbnailImage($(this), oldIndex);
 });
@@ -118,9 +131,11 @@ function chooseThumbnailImage($this, oldIndex) {
     var posY = $('#draggable').position().top;
     if (currentIndex != 0) {
         posX = posX - 20;
-    }
-        else posX = posX - 12;
-    $('#draggable').animate({ 'top': posY + 'px', 'left': posX + 'px'}, 200, function(){
+    } else posX = posX - 12;
+    $('#draggable').animate({
+        'top': posY + 'px',
+        'left': posX + 'px'
+    }, 100, function() {
         //end of animation.. if you want to add some code here
     });
     $('#highlighter').html("left new: " + oldIndex);
@@ -131,15 +146,17 @@ function chooseThumbnailImage($this, oldIndex) {
 function chooseSlideImage($this, oldIndex) {
     var classTmp = 'img-thumbnail-' + oldIndex;
     var currentIndex = slider.getCurrentSlide();
-    $this.find('img').addClass('img-visiting');
     $('img.' + classTmp).removeClass('img-visiting');
+    $this.find('img').addClass('img-visiting');
     var posX = $this.position().left;
     var posY = $('#draggable').position().top;
     if (currentIndex != 0) {
         posX = posX - 20;
-    }
-    else posX = posX - 12;
-    $('#draggable').animate({ 'top': posY + 'px', 'left': posX + 'px'}, 0, function(){
+    } else posX = posX - 12;
+    $('#draggable').animate({
+        'top': posY + 'px',
+        'left': posX + 'px'
+    }, 0, function() {
         //end of animation.. if you want to add some code here
     });
     $('#highlighter').html("left new: " + oldIndex);
@@ -147,42 +164,61 @@ function chooseSlideImage($this, oldIndex) {
     $('#info-2').html("left: " + posX);
 }
 
-$('ul.bxslider.slider-overview li .btn-zoom').click(function(){
+$('ul.bxslider.slider-overview li .btn-zoom').click(function() {
     var slideOffset = $(this).data('slide-offset');
+    oldIndexOverview = slider.getCurrentSlide();
+    slider.destroySlider();
     sliderDetail = $('.bxslider.slider-detail').bxSlider({
-    startSlide: slideOffset,
-    slideMargin: 10,
-    // controls: false,
-    speed: 10,
-    //  nextSelector: '.prev-btn',
-    // prevSelector: '.next-btn',
-    pagerType: 'short', 
-    onSlideAfter: function($slideElement, oldIndex, newIndex) {
-                sliderDetailTotal = sliderDetail.getSlideCount();
-                updateIndexSlider(newIndex, sliderDetailTotal);
-            },
-});
+        startSlide: slideOffset,
+        slideMargin: 10,
+        controls: false,
+        speed: 10,
+        //  nextSelector: '.prev-btn',
+        // prevSelector: '.next-btn',
+        pagerType: 'short',
+        onSlideAfter: function($slideElement, oldIndex, newIndex) {
+            sliderDetailTotal = sliderDetail.getSlideCount();
+            updateIndexSlider(newIndex, sliderDetailTotal);
+        },
+    });
     $('.row.first-row').hide();
     $('.row.second-row').show();
     sliderDetailTotal = sliderDetail.getSlideCount();
     // $('#box-slide-index').val(slideOffset + 1);
     // $('.total-slide').html('/' + sliderDetailTotal);
     updateIndexSlider(slideOffset, sliderDetailTotal);
-     sliderDetail.reloadSlider();
+    sliderDetail.reloadSlider();
     console.log('Id: ' + slideOffset);
 });
 
-$('ul.bxslider.slider-detail li .btn-exit').click(function(){
-    var slideOffset = $(this).data('slide-offset');
-//     var sliderDetail = $('.bxslider.slider-detail').bxSlider({
-//     startSlide: slideOffset,
-//     slideMargin: 10,
-//     speed: 10
-// });
+$('ul.bxslider.slider-detail li .btn-exit').click(function() {
+    slideIndex = $(this).data('slide-index');
+    //     var sliderDetail = $('.bxslider.slider-detail').bxSlider({
+    //     startSlide: slideOffset,
+    //     slideMargin: 10,
+    //     speed: 10
+    // });
+    sliderDetail.destroySlider();
     $('.row.first-row').show();
     $('.row.second-row').hide();
-     sliderDetail.destroySlider();
-    console.log('Id: ' + slideOffset);
+    slider = $('.bxslider.slider-overview').bxSlider({
+        pagerCustom: '#bx-pager',
+        startSlide: slideIndex,
+        // slideWidth: 100,
+        slideMargin: 10,
+        controls: false,
+        pagerType: 'full',
+        onSlideAfter: function($slideElement, oldIndex, newIndex) {
+            var liSelected = 'li-img-' + newIndex;
+            var $this = $('li.' + liSelected)
+            chooseSlideImage($this, oldIndex);
+        },
+        speed: 10
+    });
+    var liSelected = 'li-img-' + slideIndex;
+    var $this = $('li.' + liSelected)
+    chooseSlideImage($this, oldIndexOverview);
+    // console.log('Id: ' + slideOffset);
 });
 
 function updateIndexSlider(newIndex, totalIndex) {
@@ -191,22 +227,20 @@ function updateIndexSlider(newIndex, totalIndex) {
     if (newIndex == 1) {
         $('#slider-prev').addClass('deactive');
         $('#slider-next').removeClass('deactive');
-    }
-    else if (newIndex == totalIndex) {
+    } else if (newIndex == totalIndex) {
         $('#slider-next').addClass('deactive');
-         $('#slider-prev').removeClass('deactive');
-    }
-    else {
+        $('#slider-prev').removeClass('deactive');
+    } else {
         $('#slider-prev').removeClass('deactive');
         $('#slider-next').removeClass('deactive');
     }
-} 
+}
 
 $('.next-btn').click(function() {
     var currentDetail = sliderDetail.getCurrentSlide() + 1;
     if (currentDetail != sliderDetailTotal) {
         sliderDetail.goToNextSlide();
-         updateIndexSlider(currentDetail + 1, sliderDetailTotal);
+        updateIndexSlider(currentDetail + 1, sliderDetailTotal);
     }
 });
 
@@ -214,6 +248,6 @@ $('.prev-btn').click(function() {
     var currentDetail = sliderDetail.getCurrentSlide() + 1;
     if (currentDetail != 1) {
         sliderDetail.goToPrevSlide();
-         updateIndexSlider(currentDetail - 1, sliderDetailTotal);
+        updateIndexSlider(currentDetail - 1, sliderDetailTotal);
     }
-}); 
+});
